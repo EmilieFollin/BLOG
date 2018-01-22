55c9a2a2fb6c055440640cc9c4b6c614952a5ca3
Emilie Follin <32874512+EmilieFollin@users.noreply.github.com>
2018-01-22 17:22:09 +0100

    Ajout commentaire/auteur... du 22/01/2018

--- index.php
+++ index.php
@@ -3,21 +3,21 @@
 
     session_start();
 
-
-
-        if(!isset($_COOKIE["message"])){
-            setcookie("message", 1);
-
+    if(!isset($_COOKIE["message"])){
+        setcookie("message", 1);
     }
 
     $_SESSION['host'] = 'mysql:host=localhost;dbname=blog2;charset=utf8';
     $_SESSION['ndcSQL'] = 'root';
     $_SESSION['mdpSQL'] = '';
 
+    $bdd = new PDO($_SESSION['host'], $_SESSION['ndcSQL'], $_SESSION['mdpSQL']);
+
     $_SESSION['pseudo'] = '';
     $_SESSION['success']= false;
     $_SESSION['login'] = false;
     $_SESSION['userId'] = '';
+
 
     header('Location: ./assets/php/main.php');
     
